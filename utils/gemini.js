2500a0c17ad220e4a76437c75d094eaee1325a3b
gauravzyadav <gauravyadav9182@gmail.com>
2026-01-19 15:45:05 +0530

    gemini model changed

--- utils/gemini.js
+++ utils/gemini.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ 
-    model: "gemini-2.0-flash", 
+    model: "gemini-1.5-flash", 
     generationConfig: {
         temperature: 0.7,
      },
